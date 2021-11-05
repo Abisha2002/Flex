a3ca6a51728ed0c67ec254769b315f9c5b8c7367
abisha <33662@ma-web.nl>
2021-11-05 14:35:42 +0100

    myturtle

---
+++ Turtle /myTurtle.py
@@ -0,0 +1,9 @@
+import turtle    
+my_window = turtle.Screen()     
+my_window.bgcolor("#F30079") 
+my_pen = turtle.Turtle()
+my_pen.color("white")        
+for i in range(100):
+   my_pen.forward(100)           
+   my_pen.right(230)               
+turtle.done()
